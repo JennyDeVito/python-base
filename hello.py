bbff03047e4ed145185fe2f47a3c5f1b386c5e50
Jeniffer DeVito <jdevito@live.com>
2025-05-20 11:44:53 -0300

    Insercão de documentacão, metadados e condicionais para traducão de Hello World de acordo com a linguagem de sistema.

--- hello.py
+++ hello.py
@@ -1,5 +1,34 @@
 #!/usr/bin/env python3
+"""Multi language hello world
 
-#Este programa imprime Hello World
+Depending on the language in the system's configuration the program shows the
+correspoding message.
 
-print("Hello, World!")
+Usage:
+you should have the language variable properly configurated:
+
+    export LANG=pt_BR
+
+Execution:
+
+    python3 hello.py
+    or
+    ./hello.py
+
+"""
+#metada
+__version__ = "0.0.1"
+__author__ = "Jenny DeVito"
+__license__ = "Unlicense"
+
+#program
+current_language = "en_US"
+
+msg = "Hello, World!"
+
+if  current_language == "pt_BR":
+    msg = "Olá, Mundo!"
+elif current_language == "it_IT":
+    msg = "Ciao, Mondo!"
+
+print(msg)
