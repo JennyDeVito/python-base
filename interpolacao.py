b6f10de21940da0276741d1bd0d3ac8601957a3e
Jeniffer DeVito <jdevito@live.com>
2025-05-22 13:42:41 -0300

    Criado interpolacao.py para manipulacão posterior.

---
+++ interpolacao.py
@@ -0,0 +1,13 @@
+email_templ = """
+	Olá, %(nome)s
+    Tem interesse em comprar %(produto)s?
+    Este produto é ótimo para resolver
+    %(texto)s
+    Clique agora em %(link)s
+    Apenas %(quantidade)d disponíveis!
+    Preco promocional %(preco).2f
+    """
+clientes = ["Maria", "Joao", "Jenny"]
+
+for cliente in clientes:
+	print(email_templ % {"nome": cliente, "produto": "caneta", "texto": "Escrever muito bem", "link": "https://canetaslegais.com", "quantidade": 1, "preco": 50.5,} )
