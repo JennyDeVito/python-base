99bcecedb7040bb64db577a56e13f267dbb4b606
Jeniffer DeVito <jdevito@live.com>
2025-06-06 11:57:35 -0300

    Adicionado rotating file handler

--- logs.py
+++ logs.py
@@ -4,6 +4,7 @@
 
 import os
 import logging
+from logging import handlers
 
 """LOG CONFIGURATION"""
 #TODO: usar funcão
@@ -13,18 +14,28 @@ log_level = os.getenv("LOG_LEVEL", "WARNING").upper()
 #creating my own logger instance
 log = logging.Logger("Jenny", log_level)
 #stabilishing the handler
-ch = logging.StreamHandler()
+#ch = logging.StreamHandler() #sends the log message to the console
+fh = handlers.RotatingFileHandler(
+    "meulog_logs.log",
+    maxBytes=2**20, #the ideal is something around 2**20 or 1Mib
+    backupCount=10, #teacher suggested 10 but is to be evaluated
+)
 #setting the level as the debug level
-ch.setLevel(log_level)
+#ch.setLevel(log_level) #console handler
+fh.setLevel(log_level) #file handler
 #formatting the log message
 fmt = logging.Formatter(
     "%(asctime)s %(name)s %(levelname)s l:%(lineno)d "
     "f:%(filename)s: %(message)s"
 )
 #adds the formatting configuration to the console handler
-ch.setFormatter(fmt)
+#ch.setFormatter(fmt)
+#adds the formatting configuration to the file handler
+fh.setFormatter(fmt)
 #tells that my log has a console handler
-log.addHandler(ch)
+#log.addHandler(ch)
+#tells that my log has a file handler
+log.addHandler(fh)
 
 """
 #logging message examples
