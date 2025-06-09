b97e21527fdb990619724505902d620f1e17576f
Jeniffer DeVito <jdevito@live.com>
2025-06-08 21:21:45 -0300

    adicionado loop while

--- notes.py
+++ notes.py
@@ -12,7 +12,7 @@ $ python3 notes.py read tech
 ...
 """
 
-__version__ = "0.1.1"
+__version__ = "0.1.2"
 
 import os
 import sys
@@ -36,39 +36,44 @@ if not arguments:
 if arguments[0] not in cmds:
     print(f"Invalid command {arguments[0]}")
 
-#read notes
-if arguments[0] == "read":
-    #checks the number of arguments
-    if len(arguments) != 2:
-        print("Invalid number of arguments!")
-        print("read 'tagname'")
-        sys.exit(1)
+while True:
 
-    for line in open(filepath):
-        #unpacks the line into 3 variables
-        title, tag, text = line.split("\t")
-        #compares the given tag with the tags in the notebook
-        if tag.lower() == arguments[1].lower():
-            print(f"Title: {title}")
-            print(f"Text: {text}")
-            print("-" * 30)
-            print()
+    #read notes
+    if arguments[0] == "read":
+        try:
+            arg_tag = arguments[1].lower()
+        except IndexError:
+            arg_tag = input("Type the tag of your note: ").strip().lower()
+
+        #notes reading
+        for line in open(filepath):
+            #unpacks the line into 3 variables
+            title, tag, text = line.split("\t")
+            #compares the given tag with the tags in the notebook
+            if tag.lower() == arg_tag:
+                print(f"Title: {title}")
+                print(f"Text: {text}")
+                print("-" * 30)
+                print()
+                
+    #write notes
+    if arguments[0] == "new":
+        try:
+            title = arguments[1]
+        except IndexError:
+            title = input("Type the title of your note: ").strip().title()
             
-#write notes
-if arguments[0] == "new":
-    try:
-        title = arguments[1]
-    except IndexError as e:
-        print(f"[Error] {str(e)}.")
-        print("You must give a title to your note!")
-        print("Try: $ python3 notes.py new 'Title'")
-        sys.exit(1)
-    text = [
-        f"{title}",
-        input("tag: ").strip(),
-        input("text: \n").strip(),
-    ]
+        text = [
+            f"{title}",
+            input("tag: ").strip(),
+            input("text: \n").strip(),
+        ]
 
-#by convention the field separator will be \t - tsv
-    with open(filepath, "a") as file_:
-        file_.write("\t".join(text) + "\n")
+    #by convention the field separator will be \t - tsv
+        with open(filepath, "a") as file_:
+            file_.write("\t".join(text) + "\n")
+
+    cont =  input("Do you want to continue? [y]").strip().lower()
+
+    if cont != "y":
+        break
